36d4e968771ae0516db22f734b21f8a494d93e45
andreivalry <mailworm.andrei@gmail.com>
2016-04-23 00:34:19 +0700

    Signed-off-by: andreivalry

--- root/config/database.php
+++ root/config/database.php
@@ -72,8 +72,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+$status=TRUE;
+if ($status==TRUE){
 $db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'mysql.idhostinger.com',
+	'username' => 'u873577078_cbm',
+	'password' => 'o8o32o16',
+	'database' => 'u873577078_cblog',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);}elseif($status==FALSE){
+	$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => '',
@@ -94,3 +116,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+}
