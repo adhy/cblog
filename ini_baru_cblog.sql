27b1dd2b423228e53736eb6f736c3cd214bab951
1st3rben <adhytsa18@gmail.com>
2016-12-04 13:29:29 +0700

    1st3rben

--- ini_baru_cblog.sql
+++ ini_baru_cblog.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 03, 2016 at 10:13 
+-- Generation Time: Dec 04, 2016 at 07:27 
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -41,15 +41,26 @@ CREATE TABLE `cb_categories` (
 --
 
 INSERT INTO `cb_categories` (`id`, `nm_c`, `slg_c`, `id_parent`, `c_date`, `u_date`, `status`) VALUES
-(6, 'okk', 'okk', 8, '2016-06-18 10:11:52', '2016-10-01 10:00:44', '0'),
-(7, 'oksa', 'oksa', 0, '0000-00-00 00:00:00', '2016-06-19 08:34:22', '0'),
-(8, 'w', 'w', 0, '2016-06-18 10:16:04', '2016-10-01 16:02:16', '0'),
-(9, 'ok', 'ok', 0, '2016-06-18 10:18:07', '2016-06-19 12:43:28', '1'),
-(10, 'okkk', 'okkk', 0, '2016-06-19 14:43:38', '2016-06-19 08:34:39', '0'),
+(6, 'okk', 'okk', 0, '2016-06-18 10:11:52', '2016-11-30 15:50:17', '0'),
+(7, 'oksa', 'oksa', 0, '0000-00-00 00:00:00', '2016-11-30 15:49:05', '0'),
+(8, 'w', 'w', 0, '2016-06-18 10:16:04', '2016-12-02 16:49:11', '1'),
+(9, 'ok', 'ok', 21, '2016-06-18 10:18:07', '2016-12-02 16:49:34', '1'),
+(10, 'okkk', 'okkk', 0, '2016-06-19 14:43:38', '2016-11-30 15:49:11', '0'),
 (11, 'muk', 'muk', 0, '2016-06-27 05:56:58', '2016-06-26 22:56:58', '0'),
 (12, 'masuk', 'masuk', 0, '2016-06-27 05:57:08', '2016-09-22 03:37:44', '0'),
-(13, 'ff*(as)(*iiUH#@gg*&77', 'ff-as-iiuh-gg-77', 0, '2016-09-26 16:02:21', '2016-09-26 10:21:32', '0'),
-(14, 'kk&^$%ll', 'kk-ll', 0, '2016-09-26 17:21:46', '2016-09-26 10:21:46', '0');
+(14, 'kk&^$%ll', 'kk-ll', 0, '2016-09-26 17:21:46', '2016-09-26 10:21:46', '0'),
+(15, 'mlm', 'mlm', 0, '2016-10-07 16:17:43', '2016-10-07 09:17:43', '0'),
+(16, 'aaa', 'aaa', 21, '2016-10-08 14:51:32', '2016-12-02 16:03:46', '1'),
+(17, 'cccc', 'cccc', 0, '2016-10-08 14:51:32', '2016-11-30 15:49:44', '0'),
+(18, 'dddd', 'dddd', 0, '2016-10-08 14:51:32', '2016-11-30 15:49:47', '0'),
+(21, '3333', '3333', 0, '2016-10-08 14:54:25', '2016-11-30 16:10:51', '1'),
+(23, '1010', '1010', 0, '2016-10-08 20:25:05', '2016-11-30 16:03:33', '1'),
+(24, 'malam jum\\\\\\''at', 'malam-jum-at', 0, '2016-10-18 13:53:33', '2016-10-18 06:53:59', '0'),
+(27, 'jum''at', 'jum-at', 0, '2016-10-18 16:33:10', '2016-11-30 15:52:36', '0'),
+(30, 'jum''atjj', 'jum-atjj', 0, '2016-10-18 16:39:56', '2016-10-18 09:42:52', '0'),
+(31, 'jum''atjj', 'jum-atjj', 0, '2016-10-18 16:44:27', '2016-10-18 09:44:27', '0'),
+(32, 'dua tiga', 'dua-tiga', 0, '2016-11-27 15:43:36', '2016-11-30 15:52:33', '0'),
+(33, 'masuk tiga', 'masuk-tiga', 0, '2016-11-27 15:43:36', '2016-11-27 08:43:36', '0');
 
 -- --------------------------------------------------------
 
@@ -71,10 +82,10 @@ CREATE TABLE `cb_catrelation` (
 --
 
 INSERT INTO `cb_catrelation` (`id`, `id_c`, `id_cont`, `c_date`, `u_date`, `status`) VALUES
-(1, 9, 1, '2016-09-26 12:01:11', '2016-09-26 05:01:11', 0),
+(1, 9, 1, '2016-11-16 14:39:42', '2016-12-02 16:46:26', 1),
 (2, 8, 2, '2016-09-26 15:48:23', '2016-09-26 08:48:23', 0),
 (3, 9, 3, '2016-09-26 17:08:31', '2016-09-26 10:08:31', 0),
-(4, 8, 4, '2016-09-26 17:23:37', '2016-09-26 10:23:37', 0);
+(4, 9, 4, '2016-10-27 15:56:04', '2016-10-27 08:56:04', 0);
 
 -- --------------------------------------------------------
 
@@ -91,19 +102,21 @@ CREATE TABLE `cb_contents` (
   `content` text NOT NULL,
   `c_date` datetime NOT NULL,
   `u_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `views` char(255) NOT NULL,
   `status` varchar(5) NOT NULL,
-  `creator` int(5) NOT NULL
+  `creator` int(5) NOT NULL,
+  `id_cat` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cb_contents`
 --
 
-INSERT INTO `cb_contents` (`id`, `imgheader`, `title`, `slug`, `meta_content`, `content`, `c_date`, `u_date`, `status`, `creator`) VALUES
-(1, '/assets/img/style_abstract_dragon_74494_2560x1600.jpg', 'conaltmasuk = coab   + sa  coba    () coba lagi **&()manusia ++ && lagi', 'conaltmasuk-coab-sa-coba-coba-lagi-manusia-lagi', 'masuk', '<p>masuk</p>', '2016-09-26 12:01:11', '2016-10-01 16:01:57', '1', 1),
-(2, '/assets/img/patterns_lines_light_bright_glow_85560_2560x1600.jpg', 'gsaajha 780 *&* ok', 'gsaajha-780-ok', 'asa', '<p>asw</p>', '2016-09-26 15:48:23', '2016-10-01 16:01:53', '1', 1),
-(3, '', 'uue)@#_jwiue)(*$&)$@we', 'uue-jwiue-we', 'sds', '', '2016-09-26 17:08:31', '2016-10-01 16:01:52', '1', 1),
-(4, '', 'LL~!*oo', 'll-oo', 'sd', '', '2016-09-26 17:23:36', '2016-10-01 16:01:51', '1', 1);
+INSERT INTO `cb_contents` (`id`, `imgheader`, `title`, `slug`, `meta_content`, `content`, `c_date`, `u_date`, `views`, `status`, `creator`, `id_cat`) VALUES
+(1, '/assets/img/style_abstract_dragon_74494_2560x1600.jpg', 'conaltmasuk = coab   + sa  coba    () coba lagi **&()manusia ++ && lagi', 'conaltmasuk-coab-sa-coba-coba-lagi-manusia-lagi', 'masuk', '<p>masuk</p>', '2016-11-16 14:39:42', '2016-12-03 15:48:45', '0', '1', 1, 9),
+(2, '/assets/img/patterns_lines_light_bright_glow_85560_2560x1600.jpg', 'gsaajha 780 *&* ok', 'gsaajha-780-ok', 'asa', '<p>asw</p>', '2016-09-26 15:48:23', '2016-12-03 16:09:53', '0', '1', 1, 8),
+(3, '', 'uue)@#_jwiue)(*$&)$@we', 'uue-jwiue-we', 'sds', '', '2016-09-26 17:08:31', '2016-12-03 16:10:11', '0', '1', 1, 9),
+(4, '/assets/img/style_abstract_dragon_74494_2560x1600.jpg', 'LL~!*oo \\\\\\''', 'll-oo', 'sd', '<p>csc sf<img src=\\"http://localhost/cblog/assets/public/img/angel_wings_white_black_girl_3563_2560x1600.jpg?1477576545639\\" alt=\\"km jh\\" width=\\"560\\" height=\\"350\\" /></p>', '2016-10-27 15:56:04', '2016-12-03 16:10:20', '0', '1', 1, 9);
 
 -- --------------------------------------------------------
 
@@ -194,7 +207,7 @@ CREATE TABLE `cb_profile` (
 --
 
 INSERT INTO `cb_profile` (`id_user`, `nm_user`, `email`, `mopho`, `decript`, `alamat`, `img`, `c_date`, `u_date`) VALUES
-(1, 'epona', '353emperor@gmail.com', '01+33', 'cobe text', 'anonymous', '/assets/img/style_abstract_dragon_74494_2560x1600.jpg', '2016-04-30 00:00:00', '2016-09-17 16:48:30');
+(1, '3pona', 'adhytsa18@gmail.com', '01+33', 'coba text', 'anonymous', '/assets/img/2kufff.png', '2016-04-30 00:00:00', '2016-12-03 18:01:28');
 
 -- --------------------------------------------------------
 
@@ -238,18 +251,20 @@ CREATE TABLE `cb_tags` (
 --
 
 INSERT INTO `cb_tags` (`id`, `nm_t`, `slg_t`, `c_date`, `u_date`, `status`) VALUES
-(8, '34', '34', '2016-06-20 09:49:45', '2016-10-01 16:03:07', '0'),
 (9, '44', '44', '2016-06-20 09:49:45', '2016-09-22 08:48:26', '1'),
 (10, 'Satu semua', 'satu-semua', '2016-06-20 09:57:23', '2016-09-22 08:48:27', '1'),
 (15, 'qwsas  *(ee', 'qwsas-ee', '2016-09-26 16:23:30', '2016-09-26 09:30:43', '0'),
 (16, 'gsaajha 780 *&* ok', 'gsaajha-780-ok', '2016-09-26 16:23:30', '2016-09-26 09:29:23', '0'),
-(17, 'gsaajha 780 *&* ok&*s', 'gsaajha-780-oks', '2016-09-26 16:30:05', '2016-09-26 09:30:05', '0'),
+(17, 'gsaajha 780 *&* ok&*s', 'gsaajha-780-oks', '2016-09-26 16:30:05', '2016-12-01 15:05:57', '1'),
 (18, 'hag)$@gg@$kbb', 'hagggkbb', '2016-09-26 16:36:00', '2016-09-26 09:36:00', '0'),
 (19, 'H~!@#$%^&*()_+|}{\\\\\\":?\\">|<`-=][\\\\\\\\\\\\\\'';/.,ok', 'h-ok', '2016-09-26 16:36:00', '2016-09-26 09:41:03', '0'),
 (20, 'jja+_^qq**sak', 'jja_qqsak', '2016-09-26 16:36:01', '2016-09-26 09:36:01', '0'),
 (22, 'aku <p xss=removed> dan<p>', 'aku-p-xss-removed-dan-p', '2016-09-26 16:45:16', '2016-09-26 09:46:21', '0'),
 (23, 'fungsi < br> cobadh', 'fungsi-br-cobadh', '2016-09-26 16:47:46', '2016-09-26 09:55:40', '0'),
-(24, 'A~@!12S#$Syy^^k', 'a-12s-syy-k', '2016-09-26 17:22:07', '2016-09-26 10:22:53', '0');
+(24, 'A~@!12S#$Syy^^k', 'a-12s-syy-k', '2016-09-26 17:22:07', '2016-09-26 10:22:53', '0'),
+(25, 'malam rabu', 'malam-rabu', '2016-10-18 13:22:57', '2016-10-18 06:22:57', '0'),
+(26, 'malam jum\\''at', 'malam-jum-at', '2016-10-18 13:22:57', '2016-10-18 06:22:57', '0'),
+(27, 'malam minggu', 'malam-minggu', '2016-10-18 13:22:57', '2016-10-18 06:22:57', '0');
 
 -- --------------------------------------------------------
 
@@ -271,13 +286,12 @@ CREATE TABLE `cb_tagsrelation` (
 --
 
 INSERT INTO `cb_tagsrelation` (`id`, `id_tag`, `id_cont`, `c_date`, `u_date`, `status`) VALUES
-(38, 8, 1, '2016-09-26 12:01:11', '2016-09-26 05:01:11', '0'),
-(39, 9, 1, '2016-09-26 12:01:11', '2016-09-26 05:01:11', '0'),
-(40, 8, 2, '2016-09-26 15:48:23', '2016-09-26 08:48:23', '0'),
-(41, 10, 2, '2016-09-26 15:48:24', '2016-09-26 08:48:24', '0'),
-(44, 9, 3, '2016-09-26 17:08:32', '2016-09-26 10:08:32', '0'),
-(45, 10, 3, '2016-09-26 17:08:32', '2016-09-26 10:08:32', '0'),
-(46, 9, 4, '2016-09-26 17:23:37', '2016-09-26 10:23:37', '0');
+(41, 9, 2, '2016-09-26 15:48:24', '2016-12-02 16:20:26', '1'),
+(44, 17, 3, '2016-09-26 17:08:32', '2016-12-02 16:38:40', '1'),
+(45, 9, 3, '2016-09-26 17:08:32', '2016-12-02 16:19:46', '1'),
+(48, 9, 4, '2016-10-27 15:56:05', '2016-12-01 15:03:24', '1'),
+(49, 17, 1, '2016-11-16 14:39:42', '2016-12-02 16:46:27', '1'),
+(50, 10, 1, '2016-11-16 14:39:43', '2016-12-02 16:46:27', '1');
 
 -- --------------------------------------------------------
 
@@ -401,7 +415,8 @@ ALTER TABLE `cb_catrelation`
 --
 ALTER TABLE `cb_contents`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `creator` (`creator`);
+  ADD KEY `creator` (`creator`),
+  ADD KEY `id_cat` (`id_cat`);
 
 --
 -- Indexes for table `cb_img`
@@ -464,7 +479,7 @@ ALTER TABLE `mailing`
 -- AUTO_INCREMENT for table `cb_categories`
 --
 ALTER TABLE `cb_categories`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT for table `cb_catrelation`
 --
@@ -504,12 +519,12 @@ ALTER TABLE `cb_status`
 -- AUTO_INCREMENT for table `cb_tags`
 --
 ALTER TABLE `cb_tags`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `cb_tagsrelation`
 --
 ALTER TABLE `cb_tagsrelation`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT for table `mailing`
 --
@@ -530,7 +545,8 @@ ALTER TABLE `cb_catrelation`
 -- Constraints for table `cb_contents`
 --
 ALTER TABLE `cb_contents`
-  ADD CONSTRAINT `cb_contents_ibfk_2` FOREIGN KEY (`creator`) REFERENCES `cb_profile` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `cb_contents_ibfk_2` FOREIGN KEY (`creator`) REFERENCES `cb_profile` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cb_contents_ibfk_3` FOREIGN KEY (`id_cat`) REFERENCES `cb_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `cb_log`
