e45192781dffa0888d275ec06a264a804ab30e55
adhy <adhytsa18@gmail.com>
2016-12-12 23:20:53 +0700

    Update routes.php

--- root/modules/cwblog/config/routes.php
+++ root/modules/cwblog/config/routes.php
@@ -1,6 +1,7 @@
 <?php
-$route['recent'] 	= "cwblog/is_recent";
+//$route['recent'] 	= "cwblog/is_recent";
 $route['page'] 	= "cwblog/index";
 $route['page/(:any)'] 	= "cwblog/index";
 $route['recent/(:any)'] 	= "cwblog/is_recent";
+$route['recent/page/(:any)']="cwblog/is_recent";
 ?>
