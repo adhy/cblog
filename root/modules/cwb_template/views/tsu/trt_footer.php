b2324282e339d914f4b30690fcb3daab3be1e9cd
1st3rben <adhytsa18@gmail.com>
2017-01-19 19:39:50 +0700

    1st3rben

--- root/modules/cwb_template/views/tsu/trt_footer.php
+++ root/modules/cwb_template/views/tsu/trt_footer.php
@@ -12,13 +12,15 @@
     <?php if (!empty($end_model)){
       echo $end_model;
     } ?>
+    <script type="text/javascript">
+      var urlrequire = '<?php echo base_url();?>';
+    </script>
      <?php foreach($js_fott as $urljs){echo "<script src='".base_url('assets')."/private/js/".$urljs."'></script>";}?>
 
 
     <script type="text/javascript">
     var e = ' ';
     var url = '<?php echo base_url();?>';
-    var urlrequire = '<?php echo base_url();?>';
         var csfrData = {};
 csfrData['<?php echo $this->security->get_csrf_token_name(); ?>'] = '<?php echo
 $this->security->get_csrf_hash(); ?>';
