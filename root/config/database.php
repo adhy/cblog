7bd20cb15f8452bb4b3bbd13a63ae4f74c948ba6
1st3rben <adhytsa18@gmail.com>
2017-01-20 07:22:13 +0700

    1st3rben

--- root/config/database.php
+++ root/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-$typec = 'local';
+$typec = 'wan';
 if ($typec == 'local'){
 	$hostname = 'localhost';
 	$username = 'epona';
